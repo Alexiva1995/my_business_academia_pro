49aea4552272c1d69d58509197a3388cacd6a3a4
luisana <alexisjoseva95@gmail.com>
2021-06-16 17:57:13 -0400

    nuevo diseño página coaches

--- resources/views/nosotros/coaches.blade.php
+++ resources/views/nosotros/coaches.blade.php
@@ -12,116 +12,166 @@
 @endpush
 
 @section('content')
+    <div class="banner-coaches">
+        <img src="{{ asset('nosotros/afiliados/header2.jpg') }}" class="w-100" alt="...">
 
-   <div class="container-fluid courses-slider" style="background-color: #1C1D21; padding-bottom: 0px!important;">
-      <div class="container-fluid courses-slider" style="padding-bottom: 0px!important;">
-         <div id="mainSlider" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-               <div class="carousel-item  active ">
-                  <div class="overlay"></div>
-                  <img src="{{ asset('nosotros/afiliados/header2.jpg') }}" class="d-block w-100" alt="...">
-                  <div class="carousel-caption caption-show">
-                     <div class="row">
-                        <div class="col-6 mt-4 pl-0 pr-0">
-                           <img src="{{ asset ('images/mbapro-completo.png')}}" class="logo-academia-afiliados" alt="" width="300px">
-                        </div>
-                        <!--<div class="col-9">
-                           <div class="d-flex align-items-center mybussiness-title-afiliados mt-4">My Business</div>
-                           <div class="academia-title-afiliados" style="color: white; font-size: 20px; ">A c a d e m y p r o</div>
-                        </div>-->
-                     </div>
-                     <div>
-                    	   <h2 class="col-md-8 afiliados-title-two pt-4">Te damos la bienvenida a <b class="text-white font-weight-bold">uno de los <br> mejores y más geniales </b>programas de marketing de Afiliados del mundo</h2>
-                     </div>
-                  </div>
-
-                  <!--<div class="carousel-caption caption-hidden mt-4">
-                     <div class="row">
-                        <div class="col-2 float-left">
-                           <img src="{{ asset ('nosotros/afiliados/beyond-academy-150x150.png')}}" class="d-block logo-academia-afiliados" alt="" height="100px" width="100px">
-                        </div>
-                        <div class="col-9">
-                           <div class="text-left">My Business</div>
-                           <div class="text-left" style="color: white;">A c a d e m y p r o</div>
-                        </div>
-                     </div>
-                  </div>-->
-               </div>
+        <div class="banner-coaches-text">
+             <div class="row">
+                <div class="col-12 mt-5 text-center">
+                    <img src="{{ asset ('images/mbapro-completo.png')}}"  class="banner-coaches-logo">
+                </div>
+                <div class="col-12 mt-5 text-center text-white pt-4"">
+                    Te damos la bienvenida a uno de los  mejores y más geniales programas de marketing de Afiliados del mundo
+                </div>
+                <div class="col-12 mt-3 text-center">
+                    <img src="{{ asset ('images/line-blue.png')}}" class="banner-coaches-line">
+                </div>
             </div>
-         </div>
-      </div>
+        </div>
+    </div>
 
-      <div class="col-md-12" style="background-color: #fff;">
-         <div class="col-md-8 offset-md-2" style="padding: 20px 15px 40px 15px; text-align: justify;">
-            <h4>Te ofrecemos todo el conocimiento, el desarrollo de actitudes y las herramientas que necesitas para convertirte en un gran Speaker y Coach Master de My Business Academy Pro </h4>
-            <br>
-            <h5>Si ya tienes la formación o experiencia en el medio de comunicación y manejo de equipos, pero ¿quieres obtener proyección internacional? Aquí encontrarás toda la información para digitalizar tus cursos y maximizar su alcance en línea.</h5>
-            <br>
-            <h5><b>¡Haz negocio con nosotros! y ¡Súmate a nuestro equipo de entrenadores!</b></h5><br>
+    <div class="pb-5" style="background-color: #fff;">
+        <div class="text-center coaches-second-section">
+            Te ofrecemos todo el conocimiento, el desarrollo de actitudes y las herramientas que necesitas para convertirte en un gran Speaker y Coach Master de My Business Academy Pro
+            <br><br>
+            Si ya tienes la formación o experiencia en el medio de comunicación y manejo de equipos, pero ¿quieres obtener proyección internacional? Aquí encontrarás toda la información para digitalizar tus cursos y maximizar su alcance en línea.
 
-            <center><a type="button" class="btn btn-primary btn-register-header" href="{{ route('log').'?act=1' }}">REGISTRATE AQUÍ</a></center><br>
+            <div class="mt-3 p-5 coaches-second-section-text-blue">¡Haz negocio con nosotros! y ¡Súmate a nuestro equipo de entrenadores!</div>
 
-            <h5>Con la atención y el cuidado que te mereces, valoraremos tu trabajo y sobre eso daremos seguimiento a algunas posibilidades. Pero para ello, requerimos que con la formalidad que se requiere, tengas consciencia de lo siguiente: <br><br>
-            <ul>
-               <li>My Business Academy Pro te ofrece una posibilidad, la cual necesita tú mayor compromiso, responsabilidad y colaboración.</li><br>   
-               <li>Para nosotros es importante que el trabajo de todos sea valorado, por ello tenemos requisitos importantes, así como confianza en que podrás cumplir a cabalidad el objetivo que nos mueve como grupo, que sobre todo yace en la satisfacción de nuestro consumidor final, el impacto positivo que queremos dar al mundo y la trascendencia del material que ofrecemos, por ello nuestro compromiso en la calidad de todo nuestro modelo académico.</li><br>
-               <li>Te ofrecemos la exposición internacional que siempre has soñado, apalancado de nuestra comunidad en más de 80 países y con la posibilidad de seguir creciendo por el mundo entero, le daremos un tratamiento impecable a cada material, contando con el mejor equipo técnico, operativo, logístico, administrativo y de creación de contenidos para que, sumado a extraordinarias locaciones, hagamos que expreses lo mejor de ti.</li>
-            </ul></h5>
+            <center><a type="button" class="btn coaches-second-section-button-blue" href="{{ route('log').'?act=1' }}">Regístrate Aquí</a></center><br>
 
-            <div class="row pt-3">
-               <div class="col-md-2"></div>
-               <div class="col-md-8">
-                  <img src="{{ asset('nosotros/gratis-Blog/bienvenido.jpg') }}" class="card-img-top" alt="..." style="border: solid 0;">
-                  <div style="background-color: #2A91FF; color: white; padding-top: 12px; padding-bottom: 12px; text-align: center; font-weight: bold;">
-                     <a href="#" data-toggle="modal" data-target="#video1" style="text-decoration: none; color: white;"><i class="fa fa-play-circle"></i> Testimonio de José Gordo</a>
-                  </div>
-               </div>
-               <div class="col-md-2"></div>
+            <div class="mt-3">Con la atención y el cuidado que te mereces, valoraremos tu trabajo y sobre eso daremos seguimiento a algunas posibilidades. Pero para ello, requerimos que con la formalidad que se requiere, tengas consciencia de lo siguiente:</div>
+        </div>
+
+        <div class="row pl-5 pr-5 pb-5">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-12 pl-2 pr-2 pt-2">
+                <div class="p-4 coaches-second-section-card">
+                    <div class="coaches-second-section-card-img">
+                        <img src="{{ asset('images/icons/handshake.svg') }}" width="80" height="80">
+                    </div>
+                    <div class="mt-4 pb-4 coaches-second-section-card-text">
+                        My Business Academy Pro te ofrece una posibilidad, la cual necesita tú mayor compromiso, responsabilidad y colaboración.
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-
-      <div class="col-md-12" style="background-color: #fff;">
-         <div class="col-md-8 offset-md-2" style="padding: 20px 15px 40px 15px; text-align: justify;">
-            <div class="row">
-               <div class="col-md-2"></div>
-               <div class="col-md-8">
-                  <img src="{{ asset('nosotros/afiliados/live-streaming.jpg') }}" class="card-img-top" alt="..." style="border: solid 0;">
-               </div>
-               <div class="col-md-2"></div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-12 pl-2 pr-2 pt-2">
+                <div class="p-4 coaches-second-section-card">
+                    <div class="coaches-second-section-card-img">
+                        <img src="{{ asset('images/icons/shield.svg') }}" width="80" height="80">
+                    </div>
+                    <div class="mt-4 pb-4 coaches-second-section-card-text">
+                        Para nosotros es importante que el trabajo de todos sea valorado, por ello tenemos requisitos importantes, así como confianza en que podrás cumplir a cabalidad el objetivo que nos mueve como grupo, que sobre todo yace en la satisfacción de nuestro consumidor final, el impacto positivo que queremos dar al mundo y la trascendencia del material que ofrecemos, por ello nuestro compromiso en la calidad de todo nuestro modelo académico
+                    </div>
+                </div>
             </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-12 pl-2 pr-2 pt-2">
+                <div class="p-4 coaches-second-section-card">
+                    <div class="coaches-second-section-card-img">
+                        <img src="{{ asset('images/icons/worldwide.svg') }}" width="80" height="80">
+                    </div>
+                    <div class="mt-4 pb-4 coaches-second-section-card-text">
+                        Te ofrecemos la exposición internacional que siempre has soñado, apalancado de nuestra comunidad en más de 80 países y con la posibilidad de seguir creciendo por el mundo entero, le daremos un tratamiento impecable a cada material, contando con el mejor equipo técnico, operativo, logístico, administrativo y de creación de contenidos para que, sumado a extraordinarias locaciones, hagamos que expreses lo mejor de ti.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-            <br>
-            <h4>Haremos producciones a lo largo de todo el mundo y presentaremos a los mejores coaches para tu formación y desarrollo. <br><br>
-            Personas que tengan toda el conocimiento y experiencia para ofrecerte valor en lo que sea que quieras desarrollar.</h4><br>
+    <div style="background-color: #1C1E21; border: solid #707070 1px;">
+        <div class="row mt-5 pt-5 pb-5 mb-5">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <img src="{{ asset('nosotros/gratis-Blog/bienvenido.jpg') }}" class="card-img-top" alt="..." style="border: solid 0;">
+                <div style="background-color: #2A91FF; color: white; padding-top: 12px; padding-bottom: 12px; text-align: center; font-weight: bold;">
+                    <a href="#" data-toggle="modal" data-target="#video1" style="text-decoration: none; color: white;"><i class="fa fa-play-circle"></i> Testimonio de José Gordo</a>
+                </div>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
 
-            <h5>Pero ¿Qué temas puedo encontrar en MBA PRO?
-            <ul>
-               <li>Desarrollo Humano</li>   
-               <li>Finanzas</li>
-               <li>Ventas</li>
-               <li>Emprendurismo</li>
-               <li>Fintech</li>
-               <li>Real Estate </li>
-               <li>Enseñanza Aprendizaje</li>
-               <li>Bienestar Integral</li>
-            </ul><br>
+    <div style="background-color: #fff;">
+        <div class="row" style="display: flex; align-items: center;">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-12 p-5">
+                <div class="pl-5 pr-5" style="font-size: 24px; color: #696969; font-weight: bold;">Haremos producciones a lo largo de todo el mundo y presentaremos a los mejores coaches para tu formación y desarrollo.</div>
+                <div class="pl-5 pr-5 pt-4" style="font-size: 24px; color: #696969;">Personas que tengan todo el conocimiento y experiencia para ofrecerte valor en lo que sea que quieras desarrollar.</div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-12 p-5">
+                <img src="{{ asset('nosotros/afiliados/live-streaming.jpg') }}" class="card-img-top" alt="..." style="border: solid 0;">
+            </div>
+        </div>
 
-            Si tienes algún tema de valor que ofrecernos, envíanos tu propuesta y con gusto nuestro comité de talentos medirá y expondrá las posibilidades que podemos ofrecerte a través de un proceso formal de varios filtros, porque como ya lo mencionamos, nuestra principal preocupación es la calidad de la información que ofrecemos, una vez que pase todas las validaciones, te contactaremos por teléfono y correo a los datos que nos dejaste en tu formulario para proceder con una entrevista online y coordinar detalles de relación comercial, derechos de uso, producción y post producción.<br></h5>
+        <div class="row mt-4">
+            <div class="col-3"></div>
+            <div class="col-6 text-white text-center" style="background-color: #1C1D21; border-radius: 12px; font-size: 25px; font-weight: bold; padding: 10px 20px;">Pero ¿Qué temas puedo encontrar en MBA PRO?</div>
+            <div class="col-3"></div>
+        </div>
 
-            <br>
-            <center>
-               <a type="button" class="btn btn-primary btn-register-header" href="{{ route('log').'?act=1' }}">REGISTRATE AQUÍ</a><br><br>
+        <div class="row" style="padding: 8% 10%;">
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center pb-5" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/target.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Desarrollo<br> Humano 
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center pb-5" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/money.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Finanzas
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center pb-5" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/business-and-finance.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Ventas
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center pb-5" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/shuttle.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Emprendurismo
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/cloud.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Fintech
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/house.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Real State
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/online-learning.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Enseñanza <br>Aprendizaje
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 text-center" style="color: #2A91FF;">
+                <img src="{{ asset('images/icons/lotus-flower.svg') }}" width="70" height="70">
+                <div class="mt-3" style="font-size: 26px; line-height: 30px; font-weight: bold;">
+                    Bienestar <br>Integral
+                </div>
+            </div>
+        </div>
 
-               <b>Solicitamos tu empatía y paciencia durante el proceso, contamos con muchas solicitudes y el trabajo de todos es importante. 
+        <div class="text-white text-center" style="background-color: #2A91FF; padding: 8% 10%; font-size: 26px; line-height: 36px;">
+            Si tienes algún tema de valor que ofrecernos, envíanos tu propuesta y con gusto nuestro comité de talentos medirá y expondrá las posibilidades que podemos ofrecerte a través de un proceso formal de varios filtros, porque como ya lo mencionamos, nuestra principal preocupación es la calidad de la información que ofrecemos, una vez que pase todas las validaciones, te contactaremos por teléfono y correo a los datos que nos dejaste en tu formulario para proceder con una entrevista online y coordinar detalles de relación comercial, derechos de uso, producción y post producción.
+            
+            <div class="mt-5">
+                <a type="button" class="btn" style="background-color: #1C1E21; color: white; font-size: 25px; font-weight: bold; border-radius: 15px !important; padding: 10px 20px;" href="{{ route('log').'?act=1' }}">Regístrate Aquí</a>
+            </div>
+        </div>
 
-               Esperamos pronto sumarte a nuestro equipo de talentos.</b>
-            </center>
-
-         </div>
-      </div>
-   </div>
-      <div class="modal fade" id="video1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="text-center" style="background-color: #FFFFFF; padding: 8% 15%; font-size: 26px; line-height: 36px; color: #696969;">
+            Solicitamos tu empatía y paciencia durante el proceso, contamos con muchas solicitudes y el trabajo de todos es importante. Esperamos pronto sumarte a nuestro equipo de talentos.
+        </div>
+    </div>
+    
+    <div class="modal fade" id="video1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-lg" role="document">
          <div class="modal-content">
             <div class="modal-header">
