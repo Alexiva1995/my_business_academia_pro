3f4f557487fd65ff95367a7341e025e7e81e96f4
luisana <alexisjoseva95@gmail.com>
2021-06-18 15:16:57 -0400

    nuevo diseño página comisiones

--- resources/views/nosotros/commissions.blade.php
+++ resources/views/nosotros/commissions.blade.php
@@ -11,8 +11,148 @@
 @endpush
 
 @section('content')
+    <div class="banner-coaches">
+        <img src="{{ asset('nosotros/afiliados/header2.jpg') }}" class="w-100" alt="...">
 
-   <div class="container-fluid courses-slider" style="background-color: #1C1D21; padding-bottom: 0px!important;">
+        <div class="banner-coaches-text">
+            <div class="row">
+                <div class="col-12 mt-5 text-center">
+                    <img src="{{ asset ('images/mbapro-completo.png')}}"  class="banner-coaches-logo">
+                </div>
+                <div class="col-12 mt-5 text-center text-white pt-4"">
+                    Te damos la bienvenida a uno de los  mejores y más geniales programas de marketing de Afiliados del mundo
+                </div>
+                <div class="col-12 mt-3 text-center">
+                    <img src="{{ asset ('images/line-blue.png')}}" class="banner-coaches-line">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div style="background-color: #F5F5F5; padding: 20px 5%; text-align: center;">
+        <div>
+           <h1 class="text-primary font-weight-bold">Comisiones Afiliados</h1>
+        </div>
+
+        <div class="row p-5">
+            <div class="col-xl-2 col-lg-2 col-12 pl-0 pr-0">
+                <div class="card card-comisiones" style="background-color: #2A91FF!important; height: 100px;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-white comisiones-card-title nivel-body">Nivel</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-10 col-lg-10 col-12">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0 pr-0">
+                        <div class="card card-comisiones" style="background-color: #FFF!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title text-primary comisiones-card-title">Membresía <br> del Ser</h5>
+                                <hr class="com-line-blue">
+                                <p class="card-text">30% DE COMISIÓN POR CADA AFILIACIÓN</p>
+                                <p class="card-text">Aplica sobre el costo preferencial</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0 pr-0">
+                        <div class="card card-comisiones" style="background-color: #2A91FF!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title text-white comisiones-card-title">Membresía <br> del Hacer</h5>
+                                <hr class="com-line-white">
+                                <p class="card-text text-white">30% DE COMISIÓN POR CADA AFILIACIÓN</p>
+                                <p class="card-text text-white">Aplica sobre el costo preferencial</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0 pr-0">
+                        <div class="card card-comisiones" style="background-color: #FFF!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title text-primary comisiones-card-title">Membresía <br>  del Tener</h5>
+                                <hr class="com-line-blue">
+                                <p class="card-text">30% DE COMISIÓN POR CADA AFILIACIÓN</p>
+                                <p class="card-text">Aplica sobre el costo preferencial</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0 pr-0">
+                        <div class="card card-comisiones" style="background-color: #2A91FF!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title text-white comisiones-card-title">Membresía <br> del Trascender</h5>
+                                <hr class="com-line-white">
+                                <p class="card-text text-white">30% DE COMISIÓN POR CADA AFILIACIÓN</p>
+                                <p class="card-text text-white">Aplica sobre el costo preferencial</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div style="padding: 5% 25%; text-align: center;">
+        <h4 style="color: white; letter-spacing: -0.57px;">Sin importar la categoría de la membresía se afilie tu referido o si lo paga mensual, anual o vitalicio.</h4>
+    </div>
+
+    <div class="pb-5" style="background-color: white; text-align:center;">
+        <div style="padding: 5% 25%; font-size: 22px; line-height: 30px; font-weight: 400; color:#696969;">
+            Por supuesto y además del valor simbólico que le damos a tu trabajo, también lo retribuimos económicamente, ofreciéndote hasta un 10% del valor de cada membresía.
+        </div>
+
+        <div class="row pl-5 pr-5 ml-5 mr-5">
+            <div class="col-xl-2 col-lg-2 col-12">
+                <div class="card card-comisiones" style="background-color: #F2F2F2!important; height: 100px;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title comisiones-card-title nivel-body" style="color: #1C1E21; font-weight: bold;">Nivel</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-10 col-lg-10 col-12">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0">
+                        <div class="card card-comisiones" style="background-color: #2A91FF!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title text-white comisiones-card-title" style="font-weight: bold;">Membresía <br> del Ser</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0">
+                        <div class="card card-comisiones" style="background-color: #F2F2F2!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title comisiones-card-title" style="color: #1C1E21; font-weight: bold;">Membresía <br> del Hacer</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0">
+                        <div class="card card-comisiones" style="background-color: #2A91FF!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title text-white comisiones-card-title" style="font-weight: bold;">Membresía <br> del Tener</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-12 pl-0">
+                        <div class="card card-comisiones" style="background-color: #F2F2F2!important;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title comisiones-card-title" style="color: #1C1E21; font-weight: bold;">Membresía <br> del Trascender</h5>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="row pl-5 pr-5 ml-5 mr-5 pt-3">
+            <div class="col-xl-2 col-lg-2 col-12"></div>
+            <div class="col-xl-10 col-lg-10 col-12 p-3" style="background-color: #F2F2F2; border-radius: 0px 0px 15px 15px !important;">
+                <div style="font-size: 26px; font-weight: 500; color: #1C1E21;">10% DE COMISIÓN POR CADA MEMBRESÍA</div>
+
+                <div class="mt-1" style="font-size: 20px; font-weight: bold; color: #1C1E21;">Aplica sobre el costo preferencial</div>
+
+                <div class="mt-4" style="font-size: 20px; font-weight: 400; color: #2A91FF">O si lo paga mensual, anual o vitalicio</div>
+            </div>
+        </div>
+    </div>
+
+   <!--<div class="container-fluid courses-slider" style="background-color: #1C1D21; padding-bottom: 0px!important;">
       <div class="container-fluid courses-slider" style="padding-bottom: 0px!important;">
          <div id="mainSlider" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
@@ -24,27 +164,11 @@
                         <div class="col-6 mt-5 pl-0 pr-0">
                            <img src="{{ asset ('images/mbapro-completo.png')}}" class="logo-academia-afiliados" alt="" width="300px">
                         </div>
-                        <!--<div class="col-9">
-                           <div class="d-flex align-items-center mybussiness-title-afiliados mt-4">My Business</div>
-                           <div class="academia-title-afiliados" style="color: white; font-size: 20px; ">A c a d e m y p r o</div>
-                        </div>-->
                      </div>
                      <div>
                     	   <h2 class="col-md-8 afiliados-title-two pt-5">Te damos la bienvenida a <b class="text-white font-weight-bold">uno de los <br> mejores y más geniales </b>programas de marketing de Afiliados del mundo</h2>
                      </div>
                   </div>
-
-                  <!--<div class="carousel-caption caption-hidden mt-4">
-                     <div class="row">
-                        <div class="col-2 float-left">
-                           <img src="{{ asset ('nosotros/afiliados/beyond-academy-150x150.png')}}" class="d-block logo-academia-afiliados" alt="" height="100px" width="100px">
-                        </div>
-                        <div class="col-9">
-                           <div class="text-left">My Business</div>
-                           <div class="text-left" style="color: white;">A c a d e m y p r o</div>
-                        </div>
-                     </div>
-                  </div>-->
                </div>
             </div>
          </div>
@@ -155,5 +279,5 @@
             </table>
 
          </div>
-      </div>
+      </div>-->
 @endsection
